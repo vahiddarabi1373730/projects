0f6ceb7bdef30936b81e171ab7a93c977f6310e8
vahiddarabi1373730 <db.vahid1737@gmail.com>
2023-10-25 13:07:33 -0700

    progress project to email

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllServicesService } from './all-services.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private allServicesService: AllServicesService) {}
+  ngOnInit(): void {
+    this.allServicesService.checkLogin().subscribe((res: any) => {
+      this.allServicesService.setLogin(res.authenticated);
+    });
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { NavbarModule } from './navbar/navbar.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { checkLoginInterceptor } from './check-login.interceptor';
+import { CustomGuard } from './custom.guard';
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./signup/signup.module').then((m) => m.SignupModule),
+  },
+  {
+    path: 'signIn',
+    loadChildren: () =>
+      import('./sign-in/sign-in.module').then((m) => m.SignInModule),
+  },
+  {
+    path: 'signOut',
+    loadChildren: () =>
+      import('./signout/signout.module').then((m) => m.SignoutModule),
+  },
+  {
+    path: 'inbox',
+    loadChildren: () =>
+      import('./inbox/inbox.module').then((m) => m.InboxModule),
+    canActivate: [CustomGuard],
+  },
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    NavbarModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: checkLoginInterceptor,
+      multi: true,
+    },
+    CustomGuard,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
